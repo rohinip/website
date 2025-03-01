8fb2a655cf47428cfd694843b269a2288e2140e0
Rohini <rohinipandhi@gmail.com>
2025-03-01 14:45:50 -0800

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,48 @@
+import type React from "react"
+import "./globals.css"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Rohini Pandhi | Product Leader, Startup Advisor & Investor",
+  description:
+    "Rohini Pandhi is a product leader at Mercury, partner at Transparent Collective, and startup advisor and investor.",
+  icons: {
+    icon: [
+      {
+        url: "/favicon.ico",
+        sizes: "any",
+      },
+      {
+        url: "/icon.svg",
+        type: "image/svg+xml",
+      },
+    ],
+    apple: [
+      {
+        url: "/apple-touch-icon.png",
+        sizes: "180x180",
+      },
+    ],
+  },
+  manifest: "/site.webmanifest",
+    generator: 'v0.dev'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
+
+
+
+import './globals.css'
